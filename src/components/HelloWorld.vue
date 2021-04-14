b5c84995683edc3da3c491f36f445ece397b4bfc
Bogdan Oporanu <bogdan.oporanu@aurachain.ch>
2021-04-15 01:43:06 +0300

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hello">
+        <canvas ref="canvas"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelloWorld",
+    props: {
+        msg: String
+    }
+};
+</script>
+
+<style lang="scss">
+</style>
